071205cdb21a787a7aa19e36a3b9225f268b0839
Mikhail <mikhailtwere@gmail.com>
2022-06-22 01:14:21 -0600

    commit tree 5ad763a7cb2a035991b25328fcb6aeb6e4c22ad3

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    if (blocks <= 42){
+        return (42 - blocks)
+    }
+
+    if (blocks > 42){
+        return (blocks - 42)
+    }
+}
+
+function distanceFromHqInFeet(feet){
+    distanceFromHqInBlocks(feet)
+        if (feet <= 42){
+            return (42 - feet)*264
+        }
+
+        if (feet >= 42){
+            return (feet - 42)*264
+        }
+    
+}
+
+function distanceTravelledInFeet(distance, destination){
+    if (distance <= destination){
+        return (destination - distance)*264
+    }
+
+    if (destination >= destination){
+        return (distance - destination)*264
+    }
+}
+
+function calculatesFarePrice(start, end){
+    function farePrice(fare){
+        if (start <= end){
+            (end - start)*264 === fare
+            if (fare <= 400){
+            return 0
+            }
+            } else {
+                return (fare - 400)*.02
+            }
+    }
+}
